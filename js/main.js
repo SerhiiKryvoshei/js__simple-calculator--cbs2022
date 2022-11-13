29801f7fe12d47473a50efd14081ed92c25c1b33
Serhii Kryvoshei <sergey.krivoshey19@gmail.com>
2022-11-13 10:40:58 +0200

    Update algorithm

--- js/main.js
+++ js/main.js
@@ -1,30 +1,130 @@
 const screenLast = document.querySelector(".calculator__screen-last");
 const screenCurrent = document.querySelector(".calculator__screen-current");
 
-const buttons = document.querySelectorAll(".calculator__ellipse");
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector(".equals");
 
 let userInput = [];
 let screen = null;
+let stack = null;
+let stackOperation = null;
+let inputOperation = null;
+let currentOperation = null;
 
-function setHandlers() {
-	numberButtons.forEach((btn) => {
-		btn.addEventListener("click", numbersHandler);
-	});
+let shouldClearScreenCurrent = false;
+
+initializationCalculator();
+setHandlers();
+
+//#region initialization ----------------------------------------------------------
+
+function initializationCalculator() {
+	userInput = [];
+	screen = null;
+	stack = null;
+	stackOperation = null;
+	inputOperation = null;
+	screenLast.textContent = "";
+	screenCurrent.textContent = "0";
+	currentOperation = null;
 }
 
-function numbersHandler() {
+function setHandlers() {
+	document.addEventListener("keydown", keyboardInputHandler);
+
+	numberButtons.forEach((btn) => btn.addEventListener("click", numbersHandler));
+	operatorButtons.forEach((btn) =>
+		btn.addEventListener("click", operatorsHandler)
+	);
+	equalsBtn.addEventListener("click", equalsHandler);
+}
+
+//#endregion
+
+//#region Event Handlers ----------------------------------------------------------
+
+function keyboardInputHandler(e) {
+	if (e.key >= 0 && e.key <= 9) {
+		userInput.push(e.key);
+		updateScreen();
+	}
+	if (e.key === "Escape") initializationCalculator();
+	if (e.key === "Backspace" || e.key === "Delete") deleteNumber();
+}
+
+function numbersHandler(e) {
 	let num = this.outerText;
 
 	if (userInput.length > 8) return;
 
+	if (shouldClearScreenCurrent) clearScreenCurrent();
 	userInput.push(num);
 	updateScreen();
 }
 
-function updateScreen() {
-	screen = userInput.join("");
+function operatorsHandler(e) {
+	inputOperation = e.target.outerText;
+
+	if (screen == null) return;
+	if (inputOperation == "AC") {
+		initializationCalculator();
+		return;
+	}
+	if (inputOperation == "X") currentOperation = "*";
+	else currentOperation = inputOperation;
+
+	stack = screen;
+	screenLast.textContent = `${screenCurrent.textContent} ${inputOperation}`;
+	shouldClearScreenCurrent = true;
+}
+
+function equalsHandler(e) {
+	if (stack == null) return;
+
+	let result = doMath(stack, screen, currentOperation);
+	stack = null;
+	stackOperation;
+	screenLast.textContent = "";
+	updateScreen(result);
+}
+
+//#endregion
+
+//#region Service Functions -------------------------------------------------------
+
+function updateScreen(result) {
+	if (arguments.length > 0) screen = result;
+	else screen = userInput.join("");
+
 	screenCurrent.textContent = screen;
 }
 
-setHandlers();
+function deleteNumber() {
+	userInput.pop();
+	updateScreen();
+}
+
+function clearScreenCurrent() {
+	screenCurrent.textContent = "";
+	userInput = [];
+	shouldClearScreenCurrent = false;
+}
+
+function doMath(a, b, operator) {
+	a = parseFloat(a);
+	b = parseFloat(b);
+
+	switch (operator) {
+		case "+":
+			return a + b;
+		case "-":
+			return a - b;
+		case "*":
+			return a * b;
+		case "/":
+			return a / b;
+	}
+}
+
+//#endregion
